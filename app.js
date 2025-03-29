4068db134775c0a4639490254ea9f65135a60993
ibogoeska <i.bogoeskaa@gmail.com>
2025-03-29 13:25:59 +0100

    JS basic logic done, time to look it nicer with Bootstrap CSS

---
+++ app.js
@@ -0,0 +1,52 @@
+const p1Button = document.querySelector("#p1Button");
+const p2Button = document.querySelector("#p2Button");
+const resetBtn = document.querySelector("#reset");
+const p1ScoreDisplay = document.querySelector("#p1ScoreDisplay");
+const p2ScoreDisplay = document.querySelector("#p2ScoreDisplay");
+const winningScoreSelect = document.querySelector("#playTo");
+
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = 5;
+let isGameOver = false;
+
+p1Button.addEventListener("click", function () {
+  if (!isGameOver) {
+    p1Score += 1;
+    if (p1Score === winningScore) {
+      isGameOver = true;
+      p1ScoreDisplay.classList.add("winner");
+      p2ScoreDisplay.classList.add("loser");
+    }
+    p1ScoreDisplay.textContent = p1Score;
+  }
+});
+
+p2Button.addEventListener("click", function () {
+  if (!isGameOver) {
+    p2Score += 1;
+    if (p2Score === winningScore) {
+      isGameOver = true;
+      p2ScoreDisplay.classList.add("winner");
+      p1ScoreDisplay.classList.add("loser");
+    }
+    p2ScoreDisplay.textContent = p2Score;
+  }
+});
+
+winningScoreSelect.addEventListener("change", function (e) {
+  winningScore = parseInt(e.target.value);
+  reset();
+});
+
+resetBtn.addEventListener("click", reset);
+
+function reset() {
+  isGameOver = false;
+  p1Score = 0;
+  p2Score = 0;
+  p1ScoreDisplay.textContent = 0;
+  p2ScoreDisplay.textContent = 0;
+  p1ScoreDisplay.classList.remove("winner", "loser");
+  p2ScoreDisplay.classList.remove("winner", "loser");
+}
